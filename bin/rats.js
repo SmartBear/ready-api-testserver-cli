6acc50b2c52777a1b6cefb7f04ec5c39594b11b1
Ole Lensmar <ole.lensmar@smartbear.com>
2016-01-13 10:20:52 -0500

    added support for project files

--- bin/rats.js
+++ bin/rats.js
@@ -19,43 +19,80 @@ if( program.args.length == 0 ){
   return
 }
 
-var config = {}
-
-if( fs.existsSync( program.config )){
-  console.log('Reading config from ' +  program.config );
-  config = JSON.parse(fs.readFileSync(program.config))
-}
-
-if( program.username ){
-  config.username = program.username
-}
-
-if( program.password ){
-  config.password = program.password
-}
-
-if( program.server ){
-  config.server = program.server
-}
-
+var config = initConfig();
 console.log( "Using config: " + JSON.stringify(config))
 
 program.args.forEach(function (file) {
 
-  var recipe = JSON.parse( fs.readFileSync( file ))
-
-  request
-    .post( config.server + "/v1/readyapi/executions" )
-    .query( {async:false} )
-    .auth( config.username, config.password )
-    .send( recipe )
-    .end( function( err, res ){
-       console.log( "Response: " + res.status)
-       console.log( "Got response: " + JSON.stringify( res.body ))
-    })
+  if( file.substr( file.length-4).toLowerCase() == ".xml" ){
+    sendXmlRequest( file )
+  }
+  else {
+    sendRecipeRequest( file )
+  }
 });
 
+function sendXmlRequest(file) {
 
+  var project = fs.readFileSync(file)
+  var req = request
+    .post(config.server + "/v1/readyapi/executions/xml")
+    .query({async: false})
+    .auth(config.username, config.password)
+    .type( 'application/xml')
+    .send( project )
+    .end(function (err, res) {
+      console.log("Response: " + res.status)
+      console.log("Got project response: " + JSON.stringify(res.body))
+    })
+}
+
+function sendRecipeRequest(file) {
+  var recipe = JSON.parse(fs.readFileSync(file))
+
+  request
+    .post(config.server + "/v1/readyapi/executions")
+    .query({async: false})
+    .auth(config.username, config.password)
+    .send(recipe)
+    .end(function (err, res) {
+      console.log("Response: " + res.status)
+      console.log("Got response: " + JSON.stringify(res.body))
+    })
+}
+
+function listExecutions() {
+
+  request
+    .get(config.server + "/v1/readyapi/executions")
+    .auth(config.username, config.password)
+    .end(function (err, res) {
+      console.log("Response: " + res.status)
+      console.log("Got response: " + JSON.stringify(res.body))
+    })
+}
+
+function initConfig() {
+  var config = {}
+
+  if (fs.existsSync(program.config)) {
+    console.log('Reading config from ' + program.config);
+    config = JSON.parse(fs.readFileSync(program.config))
+  }
+
+  if (program.username) {
+    config.username = program.username
+  }
+
+  if (program.password) {
+    config.password = program.password
+  }
+
+  if (program.server) {
+    config.server = program.server
+  }
+  return config;
+}
 
 
 
